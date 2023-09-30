3ccdeebe35f3810b9da0c5fc821b5edc2fc8ec2b
laricris1702 <143663140+laricris1702@users.noreply.github.com>
2023-09-29 21:55:43 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,16 @@
+dicionario = {
+  "nome":"ian",
+  "id":"789",
+  "idade":"20"
+   
+
+}
+#verificar o valor da chave nome
+print(dicionario["nome"])
+
+#saber se um elemento existe
+print("nome" in dicionario)
+
+#valores do dicionario
+
+print(dicionario.values())
